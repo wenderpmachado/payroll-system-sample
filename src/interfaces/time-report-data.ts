96b8c330c38befaac5b0f742fdfa3b1e855d906c
wenderpmachado <wenderpmachado@gmail.com>
2021-10-04 20:53:28 -0300

    refactor(time-report): fix indentation

--- src/interfaces/time-report-data.ts
+++ src/interfaces/time-report-data.ts
@@ -3,8 +3,8 @@ export type TJobGroup = 'A' | 'B';
 export const JOB_GROUPS: TJobGroup[] = ['A', 'B'];
 
 export interface ITimeReportData {
-    date: Date;
-    hoursWorked: number;
-    employeeId: number;
-    jobGroup: TJobGroup;
+  date: Date;
+  hoursWorked: number;
+  employeeId: number;
+  jobGroup: TJobGroup;
 }
--- src/utils/extract-time-report-data.ts
+++ src/utils/extract-time-report-data.ts
@@ -1,23 +1,28 @@
 import { ITimeReportData, TJobGroup } from "src/interfaces/time-report-data";
 
+/**
+ * Extract the information from the time report row
+ * @param timeReportRow Partial data from TimeReport CSV
+ * @returns ITimeReportData
+ */
 export function extractTimeReportData(timeReportRow: string): ITimeReportData {
-    const [dateString, hoursWorkedString, employeeIdString, jobGroupString] = timeReportRow.split(',');
+  const [dateString, hoursWorkedString, employeeIdString, jobGroupString] = timeReportRow.split(',');
 
-    const [day, month, year] = dateString.split('/');
-    const date = new Date(
-        parseInt(year),
-        parseInt(month)-1,
-        parseInt(day)
-    );
+  const [day, month, year] = dateString.split('/');
+  const date = new Date(
+    parseInt(year),
+    parseInt(month)-1,
+    parseInt(day)
+  );
 
-    const hoursWorked = parseFloat(hoursWorkedString.trim());
-    const employeeId = parseInt(employeeIdString.trim());
-    const jobGroup = jobGroupString.trim() as TJobGroup;
+  const hoursWorked = parseFloat(hoursWorkedString.trim());
+  const employeeId = parseInt(employeeIdString.trim());
+  const jobGroup = jobGroupString.trim() as TJobGroup;
 
-    return {
-        date,
-        hoursWorked,
-        employeeId,
-        jobGroup
-    }
+  return {
+    date,
+    hoursWorked,
+    employeeId,
+    jobGroup
+  }
 }
