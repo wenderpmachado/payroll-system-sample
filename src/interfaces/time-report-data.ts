8dfc3cd67516f31ae3741267386bf0a8556f5ef6
wenderpmachado <wenderpmachado@gmail.com>
2021-10-04 20:37:09 -0300

    feat(time-report): added time report data extractor - from a row

---
+++ src/interfaces/time-report-data.ts
@@ -0,0 +1,10 @@
+export type TJobGroup = 'A' | 'B';
+
+export const JOB_GROUPS: TJobGroup[] = ['A', 'B'];
+
+export interface ITimeReportData {
+    date: Date;
+    hoursWorked: number;
+    employeeId: number;
+    jobGroup: TJobGroup;
+}
