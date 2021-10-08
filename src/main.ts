2ad9db9ff4b1a91246d5f1db1a44e49d19e79dc9
wenderpmachado <wenderpmachado@gmail.com>
2021-10-08 20:45:13 -0300

    build(core): Added dockerfile + k8s + env config + MongoDB configuration file

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+
+  await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
