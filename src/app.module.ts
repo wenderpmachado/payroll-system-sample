685a3672fb68965d79ab07c6b893d19f089a863a
wenderpmachado <wenderpmachado@gmail.com>
2021-10-08 20:47:32 -0300

    build(core): Added config  + typeorm module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { MongoDBConfigService } from './database/mongodb-config.service';
 import { PayrollsModule } from './payrolls/payrolls.module';
 
 @Module({
-  imports: [PayrollsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: MongoDBConfigService,
+    }),
+    PayrollsModule
+  ],
   controllers: [AppController],
   providers: [],
 })
