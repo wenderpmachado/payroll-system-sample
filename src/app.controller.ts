c315c8cc933abfb935448b771af2127a56501ce8
wenderpmachado <wenderpmachado@gmail.com>
2021-10-08 23:19:47 -0300

    feat(swagger): added open api (swagger) documentation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,12 @@
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Controller, Get } from '@nestjs/common';
 import { version } from '../package.json';
 
+@ApiTags('health check')
 @Controller()
 export class AppController {
   @Get()
+  @ApiResponse({ status: 200, description: 'Returns the API Version, if everything is ok'})
   getHello(): string {
     return `API Version: ${version}`;
   }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Payroll System')
+    .setDescription('A sample payroll system')
+    .setVersion('0.1.0')
+    .addTag('payrolls')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -9,6 +22,8 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  setupSwagger(app);
 
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
